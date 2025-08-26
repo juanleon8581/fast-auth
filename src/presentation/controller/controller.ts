a23372c8e32165f1435651fe2cf84fcb0e583ce1
Juan Pablo Leon <juanleon8581@gmail.com>
2025-08-26 16:05:37 -0500

    fix(controller): handle error status codes properly in auth controller

--- src/presentation/controller/controller.ts
+++ src/presentation/controller/controller.ts
@@ -16,6 +16,6 @@ export class AuthController {
     new RegisterUser(this.datasource)
       .execute(dto)
       .then((user) => res.json(user))
-      .catch((error) => res.status(400).json(error));
+      .catch((error) => res.status(error.status ?? 400).json(error));
   };
 }
