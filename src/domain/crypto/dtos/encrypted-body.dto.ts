4a543a64f52c35a006afbbe38a29ee21b1c0922a
Juan Pablo Leon <juanleon8581@gmail.com>
2025-10-20 00:51:29 -0500

    refactor(crypto): move encrypted-body interfaces to domain/crypto
    Move encrypted-body interfaces from shared location to domain/crypto directory to better organize code structure and reflect proper domain ownership

--- src/domain/crypto/dtos/encrypted-body.dto.ts
+++ src/domain/crypto/dtos/encrypted-body.dto.ts
@@ -2,7 +2,7 @@ import {
   IEncryptedBody,
   IEncryption,
   ISignature,
-} from "../../interfaces/encrypted-body.interfaces";
+} from "../interfaces/encrypted-body.interfaces";
 import { TRawJson } from "../../shared/interfaces/general.interfaces";
 
 import { ERRORS } from "@/config/strings/global.strings.json";
